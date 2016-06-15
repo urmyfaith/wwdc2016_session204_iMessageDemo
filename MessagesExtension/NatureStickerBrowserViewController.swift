a07ac287483c1d0f9937c5a94978a0d0580ce107
zx <zuoxue@qq.com>
2016-06-15 12:10:43 +0800

    basic func

---
+++ MessagesExtension/NatureStickerBrowserViewController.swift
@@ -0,0 +1,51 @@
+//
+//  NatureStickerBrowserViewController.swift
+//  NatureStickes
+//
+//  Created by 学左 on 6/15/16.
+//  Copyright © 2016 zx. All rights reserved.
+//
+
+import UIKit
+import Messages
+
+class NatureStickerBrowserViewController: MSStickerBrowserViewController {
+    
+    var stickers = [MSSticker]()
+    
+    func loadStickers() {
+        for i in 1...4 {
+            createSticker(asset: "stick0\(i)", localizedDescription: "stick0\(i)")
+        }
+    }
+    
+    func changeBackgroundColor(color: UIColor) {
+        stickerBrowserView.backgroundColor = color
+    }
+    
+    func createSticker(asset: String, localizedDescription: String) {
+        guard let stickerPath = Bundle.main().pathForResource(asset, ofType: "jpg") else {
+            print("path error", asset)
+            return
+        }
+        let stickerURL = URL(fileURLWithPath: stickerPath)
+        
+        let sticker: MSSticker
+        
+        do {
+            try sticker = MSSticker(contentsOfFileURL: stickerURL, localizedDescription: localizedDescription)
+            stickers.append(sticker)
+        } catch  {
+            print(error)
+            return
+        }
+    }
+    
+    override func numberOfStickers(in stickerBrowserView: MSStickerBrowserView) -> Int {
+        return stickers.count
+    }
+    
+    override func stickerBrowserView(_ stickerBrowserView: MSStickerBrowserView, stickerAt index: Int) -> MSSticker {
+        return stickers[index]
+    }
+}
